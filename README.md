673fc71a5479ba5be515d998e541cfc56cb07719
Cium <ye91@foxmail.com>
2019-10-23 15:07:27 +0800

    up

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# PHP AlipayAop SDK 实现
+# PHP AlipayAop SDK 实现 (https://docs.open.alipay.com)
 
 1. 支付宝转账支付接口 ( ALIPAY API: alipay.fund.trans.uni.transfer request )
+2. 支付宝资金账户资产查询接口 ( ALIPAY API: alipay.fund.account.query request )
+3. 转账业务单据查询接口 ( ALIPAY API: alipay.fund.trans.common.query request )
---
+++ src/request/AlipayFundAccountQueryRequest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace cium1\alipayaop\request;
+
+use cium1\alipayaop\RequestBase;
+use cium1\alipayaop\Request;
+
+/**
+ * 支付宝资金账户资产查询接口
+ * ALIPAY API: alipay.fund.account.query request
+ *
+ * @author auto create
+ * @since  1.0, 2019-10-16 14:30:31
+ */
+class AlipayFundAccountQueryRequest extends RequestBase implements Request
+{
+
+    public function setBizContent($bizContent)
+    {
+        $this->bizContent = $bizContent;
+        $this->apiParas["biz_content"] = $bizContent;
+    }
+
+    public function getBizContent()
+    {
+        return $this->bizContent;
+    }
+
+    public function getApiMethodName()
+    {
+        return "alipay.fund.account.query";
+    }
+
+    public function setNotifyUrl($notifyUrl)
+    {
+        $this->notifyUrl = $notifyUrl;
+    }
+
+    public function getNotifyUrl()
+    {
+        return $this->notifyUrl;
+    }
+
+    public function setReturnUrl($returnUrl)
+    {
+        $this->returnUrl = $returnUrl;
+    }
+
+    public function getReturnUrl()
+    {
+        return $this->returnUrl;
+    }
+
+    public function getApiParas()
+    {
+        return $this->apiParas;
+    }
+
+    public function getTerminalType()
+    {
+        return $this->terminalType;
+    }
+
+    public function setTerminalType($terminalType)
+    {
+        $this->terminalType = $terminalType;
+    }
+
+    public function getTerminalInfo()
+    {
+        return $this->terminalInfo;
+    }
+
+    public function setTerminalInfo($terminalInfo)
+    {
+        $this->terminalInfo = $terminalInfo;
+    }
+
+    public function getProdCode()
+    {
+        return $this->prodCode;
+    }
+
+    public function setProdCode($prodCode)
+    {
+        $this->prodCode = $prodCode;
+    }
+
+    public function setApiVersion($apiVersion)
+    {
+        $this->apiVersion = $apiVersion;
+    }
+
+    public function getApiVersion()
+    {
+        return $this->apiVersion;
+    }
+
+    public function setNeedEncrypt($needEncrypt)
+    {
+        $this->needEncrypt = $needEncrypt;
+    }
+
+    public function getNeedEncrypt()
+    {
+        return $this->needEncrypt;
+    }
+
+}
